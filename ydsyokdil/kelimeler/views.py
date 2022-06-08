45b8739599146d8f5de5b091a8dba6f226a12bc6
alierbey <alierbey@gmail.com>
2022-06-08 09:21:50 +0300

    bootcamp_04

--- ydsyokdil/kelimeler/views.py
+++ ydsyokdil/kelimeler/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-# from . models import Kelime
-
-# Create your views here.
 
 def kelime_list(request):
-    # kelimeler = Kelime.object.all()
-
-    # context = {
-    #     'kelimeler' : kelimeler
-    # }
-
-
     return render(request, "kelimeler.html")
